1f3967b158dd13ec8ba96a6510dbc554c5a42d58
Jason McKee <jasonmckee@live.com>
2023-02-10 13:35:50 -0600

    Updates

---
+++ precache-manifest.452761c9bbedbb886a44440b9d294b95.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9e837607fa6cfa30f588fb07a6f6418c",
+    "url": "./index.html"
+  },
+  {
+    "revision": "b35b53388b3d773cb1d9",
+    "url": "./static/css/main.373fd4ee.chunk.css"
+  },
+  {
+    "revision": "4810aa579d1969f3a925",
+    "url": "./static/js/2.4e727be8.chunk.js"
+  },
+  {
+    "revision": "117d65892edc8adcd29d4a0d57b5a80a",
+    "url": "./static/js/2.4e727be8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b35b53388b3d773cb1d9",
+    "url": "./static/js/main.22899947.chunk.js"
+  },
+  {
+    "revision": "52b6c3759730a466b276",
+    "url": "./static/js/runtime-main.8bb44f94.js"
+  },
+  {
+    "revision": "3ce357e71a836a86691b904396a8955c",
+    "url": "./static/media/cta-illustration.3ce357e7.svg"
+  },
+  {
+    "revision": "ea6949039d6554ebf2cf109f333f5811",
+    "url": "./static/media/illustration-section-01.ea694903.svg"
+  },
+  {
+    "revision": "63d0555d90c053eea1a1a7469d004661",
+    "url": "./static/media/illustration-section-02.63d0555d.svg"
+  },
+  {
+    "revision": "d42a005c98ea695e66bd28d10c54c7ad",
+    "url": "./static/media/ribbit-logo.d42a005c.png"
+  }
+]);
