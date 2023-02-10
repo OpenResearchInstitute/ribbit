042ffce0508494390e7a847ef260d5d4e928335b
Jason McKee <jasonmckee@live.com>
2023-02-10 13:38:31 -0600

    Updates

---
+++ precache-manifest.9a85a6e24909a17f10ad25266920a543.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f456ad934c1eeb45ad12f9604aa644a5",
+    "url": "/ribbit/index.html"
+  },
+  {
+    "revision": "b6ca7dada7dd7652f5c3",
+    "url": "/ribbit/static/css/main.e56cfc78.chunk.css"
+  },
+  {
+    "revision": "4810aa579d1969f3a925",
+    "url": "/ribbit/static/js/2.4e727be8.chunk.js"
+  },
+  {
+    "revision": "117d65892edc8adcd29d4a0d57b5a80a",
+    "url": "/ribbit/static/js/2.4e727be8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b6ca7dada7dd7652f5c3",
+    "url": "/ribbit/static/js/main.975d014b.chunk.js"
+  },
+  {
+    "revision": "db88983f4d8583bc51d9",
+    "url": "/ribbit/static/js/runtime-main.911e9935.js"
+  },
+  {
+    "revision": "3ce357e71a836a86691b904396a8955c",
+    "url": "/ribbit/static/media/cta-illustration.3ce357e7.svg"
+  },
+  {
+    "revision": "ea6949039d6554ebf2cf109f333f5811",
+    "url": "/ribbit/static/media/illustration-section-01.ea694903.svg"
+  },
+  {
+    "revision": "63d0555d90c053eea1a1a7469d004661",
+    "url": "/ribbit/static/media/illustration-section-02.63d0555d.svg"
+  },
+  {
+    "revision": "d42a005c98ea695e66bd28d10c54c7ad",
+    "url": "/ribbit/static/media/ribbit-logo.d42a005c.png"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "./precache-manifest.452761c9bbedbb886a44440b9d294b95.js"
+  "/ribbit/precache-manifest.9a85a6e24909a17f10ad25266920a543.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("./index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/ribbit/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
