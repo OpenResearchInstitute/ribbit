25d6f40f708ad46d4dfaf5b1750f2604a3ef0c5a
Jason McKee <jasonmckee@live.com>
2023-08-17 10:22:44 -0500

    add DEFCON Aug 2023 poster

--- src/components/sections/Learn.js
+++ src/components/sections/Learn.js
@@ -64,6 +64,22 @@ const Learn = ({
                 data-reveal-container=".split-item"
               >
                 <div className="text-xxs text-color-primary fw-600 tt-u mb-8"></div>
+                <h3 className="mt-0 mb-12">DEFCON August 2023</h3>
+                <p className="m-0">
+                  <a href="Ribbit DEFCON Algo Poster Aug 2023.pdf" download>
+                    Download
+                  </a>{" "}
+                  the pdf poster.
+                </p>
+              </div>
+            </div>
+
+            <div className="split-item">
+              <div
+                className="split-item-content center-content-mobile reveal-from-left"
+                data-reveal-container=".split-item"
+              >
+                <div className="text-xxs text-color-primary fw-600 tt-u mb-8"></div>
                 <h3 className="mt-0 mb-12">DEFCON August 2022</h3>
                 <p className="m-0">
                   <a href="RibbitPoster-July2022.pdf" download>
